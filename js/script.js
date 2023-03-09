a43fd099ab558fa59fefa41a1e5f512f5c32b96a
Muneerat <oladipomunirat@gmail.com>
2023-03-09 08:45:13 +0100

    implement the feature product

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 const nav_item = document.querySelectorAll(".nav-link");
 const bottom = document.querySelectorAll(".bottom");
-// nav_item.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   bottom.style.display = "block";
-// });
 const links = document.querySelectorAll(".my-link");
 
 nav_item.forEach(function (link) {
@@ -14,4 +10,24 @@ nav_item.forEach(function (link) {
 
 function hover() {
   document.querySelector(".imgs").src = "/img/alarm.png";
+}
+//Redirecting image to a new page FUNCTION
+
+const hoverImg = document.querySelectorAll(".featured-img");
+// hoverImg.forEach(function (e) {
+//   e.addEventListener("click", clickImg);
+// });
+function clickImg(imageSrc) {
+  // let src = e.target.src;
+  const productAdd = document.querySelector(".product-add");
+  let addedImg = document.querySelector(".add-img");
+  var newImg = document.createElement("img");
+  newImg.src = imageSrc.src;
+  document.body.appendChild(newImg);
+  window.location.href = "/product-detail.html";
+  // addedImg.innerHTML = `
+  //  <h1>hello</h1>
+  // `;
+  // productAdd.src = window.location.href + "alarm.png";
+  console.log("hello");
 }
